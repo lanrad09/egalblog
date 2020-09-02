2e1a8c1e4a721d8f3abe0397cec5fe41c64063f0
lanrad09 <lanrad09@gmail.com>
2020-09-02 11:30:18 +0200

    uninstall bcrypt and installed bcryptjs

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const { check, validationResult} = require("express-validator");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
